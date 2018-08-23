a9f2acf6b7c584f027b68e3c36f6a88a40c32342
Amin Shah Gilani <amin@gilani.me>
2018-08-23 07:28:42 +0500

    Update layout and add push script to deploy

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,128 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import {Link} from 'gatsby'
 
 import Layout from '../components/layout'
 
-const IndexPage = () => (
-  <Layout>
-    <div className="container">
-      <div class="columns">
-        <div class="column">
-          First column
+const IndexPage = () => (<Layout>
+  <div className="container">
+    <div className="columns">
+      <div className="column">
+
+        <div class="content">
+          <h2>Constitution</h2>
+          <p>Full text and amendments</p>
         </div>
-        <div class="column">
-          Second column
+        <div className="columns">
+          <div className="column is-3 content">
+            <ol>
+              <li>Lorem Ipsum non Sequitor</li>
+              <li>Lorem Ipsum non Sequitor</li>
+              <li>Lorem Ipsum non Sequitor</li>
+              <li>Lorem Ipsum non Sequitor</li>
+              <li>Lorem Ipsum non Sequitor</li>
+              <li>Lorem Ipsum non Sequitor</li>
+              <li>Lorem Ipsum non Sequitor</li>
+            </ol>
+          </div>
+          <div className="column is-3 content">
+            <ol>
+              <li>Lorem Ipsum non Sequitor</li>
+              <li>Lorem Ipsum non Sequitor</li>
+              <li>Lorem Ipsum non Sequitor</li>
+              <li>Lorem Ipsum non Sequitor</li>
+              <li>Lorem Ipsum non Sequitor</li>
+              <li>Lorem Ipsum non Sequitor</li>
+              <li>Lorem Ipsum non Sequitor</li>
+            </ol>
+          </div>
         </div>
-        <div class="column">
-          Third column
+
+        <div class="content">
+          <h2>Federal Legislature</h2>
+          <p>Acts passed by the National Assembly. Applicable to all of Pakistan.</p>
+          <div className="columns">
+            <div className="column is-3 content">
+              <ol>
+                <li>Lorem Ipsum non Sequitor</li>
+                <li>Lorem Ipsum non Sequitor</li>
+                <li>Lorem Ipsum non Sequitor</li>
+                <li>Lorem Ipsum non Sequitor</li>
+                <li>Lorem Ipsum non Sequitor</li>
+                <li>Lorem Ipsum non Sequitor</li>
+                <li>Lorem Ipsum non Sequitor</li>
+              </ol>
+            </div>
+            <div className="column is-3 content">
+              <ol>
+                <li>Lorem Ipsum non Sequitor</li>
+                <li>Lorem Ipsum non Sequitor</li>
+                <li>Lorem Ipsum non Sequitor</li>
+                <li>Lorem Ipsum non Sequitor</li>
+                <li>Lorem Ipsum non Sequitor</li>
+                <li>Lorem Ipsum non Sequitor</li>
+                <li>Lorem Ipsum non Sequitor</li>
+              </ol>
+            </div>
+          </div>
         </div>
-        <div class="column">
-          Fourth column
+
+        <div class="content">
+          <h2>Provincial Legislature</h2>
+          <p>Acts passed by the Provincial Assemblies. Applicable to all their respective provinces.</p>
+        </div>
+        <div className="columns">
+          <div className="column is-3 content">
+            <h3>Punjab</h3>
+            <ol>
+              <li>Lorem Ipsum non Sequitor</li>
+              <li>Lorem Ipsum non Sequitor</li>
+              <li>Lorem Ipsum non Sequitor</li>
+              <li>Lorem Ipsum non Sequitor</li>
+              <li>Lorem Ipsum non Sequitor</li>
+              <li>Lorem Ipsum non Sequitor</li>
+              <li>Lorem Ipsum non Sequitor</li>
+            </ol>
+          </div>
+          <div className="column is-3 content">
+            <h3>Sindh</h3>
+            <ol>
+              <li>Lorem Ipsum non Sequitor</li>
+              <li>Lorem Ipsum non Sequitor</li>
+              <li>Lorem Ipsum non Sequitor</li>
+              <li>Lorem Ipsum non Sequitor</li>
+              <li>Lorem Ipsum non Sequitor</li>
+              <li>Lorem Ipsum non Sequitor</li>
+              <li>Lorem Ipsum non Sequitor</li>
+            </ol>
+          </div>
+          <div className="column is-3 content">
+            <h3>Balochistan</h3>
+            <ol>
+              <li>Lorem Ipsum non Sequitor</li>
+              <li>Lorem Ipsum non Sequitor</li>
+              <li>Lorem Ipsum non Sequitor</li>
+              <li>Lorem Ipsum non Sequitor</li>
+              <li>Lorem Ipsum non Sequitor</li>
+              <li>Lorem Ipsum non Sequitor</li>
+              <li>Lorem Ipsum non Sequitor</li>
+            </ol>
+          </div>
+          <div className="column is-3 content">
+            <h3>KPK</h3>
+            <ol>
+              <li>Lorem Ipsum non Sequitor</li>
+              <li>Lorem Ipsum non Sequitor</li>
+              <li>Lorem Ipsum non Sequitor</li>
+              <li>Lorem Ipsum non Sequitor</li>
+              <li>Lorem Ipsum non Sequitor</li>
+              <li>Lorem Ipsum non Sequitor</li>
+              <li>Lorem Ipsum non Sequitor</li>
+            </ol>
+          </div>
         </div>
       </div>
     </div>
-  </Layout>
-)
+  </div>
+</Layout>)
 
 export default IndexPage
